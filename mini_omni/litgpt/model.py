42993d66574bfa7d40251ef09d3878cdb47a690e
chy <cheny539@gmail.com>
2024-11-27 17:17:32 +0800

    [FIX] remove assert

--- mini_omni/litgpt/model.py
+++ mini_omni/litgpt/model.py
@@ -106,7 +106,6 @@ class GPT(nn.Module):
         for j in range(len(T)):
             if task[j] != "T1T2" and task[j] != "T1A2": # 有audio input的任务才替换
                 for i in range(7):
-                    assert input_ids[i][j, 1 : T[j] + 1, :].shape == audio_feature[j][: T[j]].shape, f"audio_feature shape mismatch, {input_ids[i][j, 1 : T[j] + 1, :].shape} != {audio_feature[j][: T[j]].shape}"
                     _seq_len = T[j] if T[j] == audio_feature[j].shape[0] else T[j] - 1
                     input_ids[i][j, 1 : _seq_len + 1, :] = audio_feature[j][: _seq_len].clone()
             else:
